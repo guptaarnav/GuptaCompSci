3c3fb876435d84f8ffc51ed879339af922468591
arnav <arnav@akshayas-air.hsd1.pa.comcast.net>
2017-12-14 20:54:41 -0500

    constructor and inputDimension()

--- src/MagicSquare.java
+++ src/MagicSquare.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * 
  */
@@ -7,13 +11,28 @@
  *
  */
 public class MagicSquare {
-
-	/**
-	 * @param args
-	 */
+	private int[][] square;
+	
+	public MagicSquare(int dimension) {
+		square = new int[dimension][dimension];
+	}
+	
+	public int inputDimension() throws IOException {
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Enter odd-numbered dimension for Magic Square: ");
+		int dimension = new Integer(input.readLine());
+		
+		while (dimension%2!=0) {
+			System.out.print("Enter correct odd-numbered dimension for Magic Square: ");
+			dimension = new Integer(input.readLine());
+		}
+		
+		return dimension;
+	}
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
