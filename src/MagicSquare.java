1f4e200b9909d7f944610cd3014cd51e74c8cfb9
arnav <arnav@akshayas-air.hsd1.pa.comcast.net>
2017-12-14 20:46:51 -0500

    creation of MagicSquare Class

---
+++ src/MagicSquare.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author arnav
+ *
+ */
+public class MagicSquare {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
