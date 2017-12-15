c8bc51a9b20bb6a6b03b1d699e345428a2de04bc
arnav <arnav@akshayas-air.hsd1.pa.comcast.net>
2017-12-14 21:35:42 -0500

    checkCols() and checkRows()

--- src/MagicSquare.java
+++ src/MagicSquare.java
@@ -22,7 +22,7 @@ public class MagicSquare {
 		System.out.print("Enter odd-numbered dimension for Magic Square: ");
 		int dimension = new Integer(input.readLine());
 		
-		while (dimension%2!=0) {
+		while (dimension%2==0) {
 			System.out.print("Enter correct odd-numbered dimension for Magic Square: ");
 			dimension = new Integer(input.readLine());
 		}
@@ -50,8 +50,15 @@ public class MagicSquare {
 		for (int target = 1; target<=this.square.length*this.square.length; target++) {
 			this.insertNumber(target, currentRow, currentCol);
 			
-			currentRow-=1;
-			currentCol+=1;
+			
+			
+			if(target%this.square.length==0) {
+				currentRow++;
+			}
+			else {
+				currentRow--;
+				currentCol++;
+			}
 			if (currentRow<0) {
 				currentRow = this.square.length-1;
 			}
@@ -63,14 +70,44 @@ public class MagicSquare {
 		
 	}
 	
-	public void insertNumber(int target, int row, int col) {
+	private void insertNumber(int target, int row, int col) {
 		this.square[row][col] = target;
 	}
 	
 	public static void main(String[] args) throws IOException{
 		MagicSquare square1 = new MagicSquare(MagicSquare.inputDimension());
+		square1.populateSquare();
 		
 		System.out.println(square1);
 	}
-
+	
+	public int checkRows() {
+		int[] rowSums = new int[this.square.length];
+		for (int row = 0; row < this.square.length; row++) {
+			for (int col = 0; col < this.square.length; col++) {
+				rowSums[row] += this.square[row][col];
+			}
+		}
+		for (int whichSum = 1; whichSum<rowSums.length; whichSum++) {
+			if (rowSums[0]!=rowSums[whichSum]) {
+				return -1;
+			}
+		}
+		return rowSums[0];
+	}
+	
+	public int checkCols() {
+		int[] colSums = new int[this.square.length];
+		for (int col = 0; col < this.square.length; col++) {
+			for (int row = 0; row < this.square.length; row++) {
+				colSums[col] += this.square[row][col];
+			}
+		}
+		for (int whichSum = 1; whichSum<colSums.length; whichSum++) {
+			if (colSums[0]!=colSums[whichSum]) {
+				return -1;
+			}
+		}
+		return colSums[0];
+	}
 }
